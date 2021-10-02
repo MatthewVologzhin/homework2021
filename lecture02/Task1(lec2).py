de3563c00ad84b6b84c2e193a90e2ba2d3775430
MatthewVologzhin <matvey.vologzhin@mail.ru>
2021-10-03 00:12:35 +0700

    Change 03.10.2021

---
+++ lecture02/Task1(lec2).py
@@ -0,0 +1,27 @@
+#Добавленные модули
+import math as m
+
+#Задание переменных
+print("Введите длину стороны A:")
+a=input()
+
+print("Введите длину стороны B:")
+b=input()
+
+print("Введите угол между A и B:")
+angle=input()
+
+#Программа
+if str.isdigit(a) and str.isdigit(b) and str.isdigit(angle):
+
+    a=int(a)
+    b=int(b)
+    angle=int(angle)
+    c=m.sqrt(a**2+b**2-2*a*b*m.cos(m.radians(angle)))
+    print("Длина третьей стороны:",c)
+
+else:
+    print("Ошибка! Введите правильные числа.")
+input()
+
+        
